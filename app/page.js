00a9c945967200ebdd86b2073668722012188ccf
deveripon <developershahadathussainripon@gmail.com>
2024-09-21 18:43:49 +0600

    added git and github

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const page = () => {
     return (
-        <div className='bg-gray-800 w-screen h-screen flex justify-center items-center'>
+        <div className='bg-gray-800 w-screen h-screen flex flex-col gap-5 justify-center items-center'>
             <Link
                 className='bg-gray-100 px-6 py-3 rounded'
                 href='/docs'>
-                👉 Docs
+                👉 Javascript Documentation
             </Link>
         </div>
     );
